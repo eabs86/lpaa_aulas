aa18da19f047143bdff8ae4408170898a6225c76
Emmanuel Andrade <emmanuel.andrade@gmail.com>
2023-11-21 14:19:38 -0300

    atualização do repo

--- python_basico/aula_7.py
+++ python_basico/aula_7.py
@@ -15,45 +15,39 @@ Trabalhando com Funções
 
 """
 
-# Funções sem retorno
-
-
+# Função que exibe uma mensagem de boas-vindas padrão
 def exibir_mensagem():
     print("Seja bem vindo!")
 
-
+# Função que exibe uma mensagem de boas-vindas com um nome específico
 def exibir_mensagem_2(nome):
     print(f"Seja bem vindo {nome}!")
 
-
+# Função que exibe uma mensagem de boas-vindas com um nome (opcionalmente "Anônimo" se não for fornecido)
 def exibir_mensagem_3(nome="Anônimo"):
     print(f"Seja bem vindo {nome}!")
 
-
+# Chamada das funções de exibição de mensagens
 exibir_mensagem()
 exibir_mensagem_2("Fulano")
 exibir_mensagem_3()
 exibir_mensagem_3(nome="Ciclano")
 
-
-# Funções com retorno
-
+# Função que calcula a soma de uma lista de números
 def calcular_total(numeros):
     return sum(numeros)
 
-
+# Função que retorna o antecessor e o sucessor de um número
 def retorna_antecessor_e_sucessor(numero):
     antecessor = numero - 1
     sucessor = numero + 1
     return antecessor, sucessor
 
-
+# Chamadas das funções com retorno
 calcular_total([10, 20, 34])  # 64
 retorna_antecessor_e_sucessor(10)  # (9, 11)
 
-# *args e **kwargs
-
-
+# Função que exibe um poema com informações de data, texto e metadados
 def exibir_poema(data_extenso, *args, **kwargs):
     texto = "\n".join(args)
     meta_dados = "\n".join(
@@ -61,53 +55,44 @@ def exibir_poema(data_extenso, *args, **kwargs):
     mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
     print(mensagem)
 
-
+# Chamada da função com argumentos variáveis
 exibir_poema("Zen of Python", "Beautiful is better than ugly.",
              autor="Tim Peters", ano=1999)
 
-
-# positional only
+# Função com argumentos posicionais apenas
 def criar_carro_po(modelo, ano, placa, /, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 
-
+# Chamadas da função com argumentos posicionais e de palavra-chave
 criar_carro_po("Palio", 1999, "ABC-1234", marca="Fiat",
             motor="1.0", combustivel="Gasolina")  # válido
-
+# A seguinte chamada é inválida devido ao uso incorreto de argumentos de palavra-chave
 criar_carro_po(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat",
             motor="1.0", combustivel="Gasolina")  # inválido
 
-
-# keyword only
+# Função com argumentos de palavra-chave apenas
 def criar_carro_ko(*, modelo, ano, placa, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 
-
+# Chamadas da função com argumentos de palavra-chave
 criar_carro_ko(modelo="Palio", ano=1999, placa="ABC-1234",
             marca="Fiat", motor="1.0", combustivel="Gasolina")  # válido
+# A seguinte chamada é inválida devido ao uso incorreto de argumentos posicionais
 criar_carro_ko("Palio", 1999, "ABC-1234", marca="Fiat",
             motor="1.0", combustivel="Gasolina")  # inválido
 
-# positional only e keword only
-
-
+# Função com argumentos posicionais e de palavra-chave
 def criar_carro_complete(modelo, ano, placa, /, *, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 
-
+# Chamadas da função com argumentos posicionais e de palavra-chave
 criar_carro_complete("Palio", 1999, "ABC-1234", marca="Fiat",
             motor="1.0", combustivel="Gasolina")  # válido
+# A seguinte chamada é inválida devido ao uso incorreto de argumentos posicionais
 criar_carro_complete(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat",
             motor="1.0", combustivel="Gasolina")  # inválido
 
-
-"""
-
-Trabalhando com POO
-
-"""
-
-
+# Classe que representa uma calculadora simples
 class Calculadora:
     def __init__(self):
         self.valor = 0
@@ -121,14 +106,14 @@ class Calculadora:
     def obter_resultado(self):
         return self.valor
 
-
+# Criar uma instância da classe Calculadora
 calculadora = Calculadora()
 calculadora.somar(5)
 calculadora.subtrair(3)
 resultado = calculadora.obter_resultado()
 print("O resultado da calculadora é:", resultado)
 
-
+# Classe que representa uma bicicleta
 class Bicicleta:
     def __init__(self, cor, modelo, ano, valor, aro=18):
         self.cor = cor
